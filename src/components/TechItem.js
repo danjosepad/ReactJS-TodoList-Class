4f89df64e9dfb61705c78da38182aaa4b9e50585
Daniel Padilha <daniel.padilha@yaman.com.br>
2020-03-05 20:44:33 -0300

    improvement: propTypes added on component

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TechItem({ tech, handleDelete }) {
   return (
@@ -11,4 +12,9 @@ function TechItem({ tech, handleDelete }) {
 TechItem.defaultProps = {
   tech: 'Oculto'
 }
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  handleDelete: PropTypes.func.isRequired
+};
+
 export default TechItem;
