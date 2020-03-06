ec87925203f708e9333e84d12c0acd36dc646cd2
Daniel Padilha <danielpadilhapessoal@outlook.com>
2020-03-05 21:16:35 -0300

    improvement: React lifecycle examples added

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,11 +5,27 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.JS',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
+  }
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs)})
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {
+
   }
 
   handleInputChange = e => {
@@ -37,7 +53,7 @@ class TechList extends Component {
           
         </ul>
         <input 
-          type="text" value={this.newTech} 
+          type="text"
           onChange={this.handleInputChange} 
           value={this.state.newTech}  
         />
